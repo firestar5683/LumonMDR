7a80bbc81602b222ae439165708d5dfa13db78fe
andrewchilicki <andrewchilx@gmail.com>
2025-02-10 13:50:03 -0500

    Cleanup files

--- compile_build_copy_pi.sh
+++
@@ -1,5 +0,0 @@
-rm -rf build
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=cmake/armv8-rpi3-linux-gnueabihf.cmake -DCROSS_COMPILE_SYSROOT=/home/andrewc/chroots/rpi-buster-armhf
-cmake --build build -j$(nproc)
-scp build/MDR_Severance pi@192.168.0.219:~/MDR/
-scp -r /home/andrewc/ProjectFiles/MDRSeverance/MDRSeverance/cmake-build-debug/assets/ pi@192.168.0.219:~/MDR/
--- compile_build_pi.sh
+++
@@ -1,3 +0,0 @@
-rm -rf build
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=cmake/armv8-rpi3-linux-gnueabihf.cmake -DCROSS_COMPILE_SYSROOT=/home/andrewc/chroots/rpi-buster-armhf
-cmake --build build -j$(nproc)
